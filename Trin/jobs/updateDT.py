002ae0426c6e35ee8c9fb181ba3d742807711e4a
zakneifian <zakneifian@gmail.com>
2018-04-12 19:26:47 +0200

    update plot

--- Trin/jobs/updateDT.py
+++ Trin/jobs/updateDT.py
@@ -5,5 +5,5 @@ from utils import SaveLoadObj
 def updateDT(bot, job):
     DatePrice = SaveLoadObj.load_obj("DTlist")
     request = requests.get('https://s3.amazonaws.com/dolartoday/data.json').json()
-    DatePrice.append({"date": datetimeVen(), "price": request["USD"]["dolartoday"]})
+    DatePrice.append({"date": datetimeVen(), "DolarToday": request["USD"]["dolartoday"]})
     SaveLoadObj.save_obj(DatePrice, "DTlist")
