caab51f1fd98e7b0053fa4eb192bf4d4c74d2c60
zakneifian <zakneifian@gmail.com>
2018-04-13 20:23:14 +0200

    update the day before for plots

--- Trin/jobs/updateDT.py
+++ Trin/jobs/updateDT.py
@@ -1,9 +1,10 @@
 from utils import SaveLoadObj
 from utils.VET import datetimeVen
 from utils.getDT import getDT
+import datetime
 
 
 def updateDT(bot, job):
     DatePrice = SaveLoadObj.load_obj("DTlist")
-    DatePrice.append({"date": datetimeVen(), "DolarToday": getDT()})
+    DatePrice.append({"date": datetimeVen() - datetime.timedelta(days=1), "DolarToday": getDT()})
     SaveLoadObj.save_obj(DatePrice, "DTlist")
--- Trin/jobs/updateLBTC.py
+++ Trin/jobs/updateLBTC.py
@@ -1,12 +1,13 @@
 from utils import SaveLoadObj
 from utils.USDVEF import getDollar
 from utils.VET import datetimeVen, timeVen
+import datetime
 
 
 def updateLBTC(bot, job):
     if job.context == "24":
         DatePrice = SaveLoadObj.load_obj("LBTClist")
-        DatePrice.append({"date": datetimeVen(), "LocalBitcoins": getDollar('24')})
+        DatePrice.append({"date": datetimeVen() - datetime.timedelta(days=1), "LocalBitcoins": getDollar('24')})
         SaveLoadObj.save_obj(DatePrice, "LBTClist")
     elif job.context == "1":
         if timeVen().hour == 0:
