e707a78e569a5aeb52a8fbddffcdffbeac53e0a1
zakneifian <zakneifian@gmail.com>
2018-04-15 05:40:03 +0200

    try except calc dolar

--- Trin/jobs/CalcDollar.py
+++ Trin/jobs/CalcDollar.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Dictionary containing the average values of the rate between USD and VEF and also the time
 dollarDic = {}
@@ -6,7 +7,11 @@ dollarDic = {}
 # This function requests the json data from LocalBitcoins, calculates the rate between
 # USD and VEF and returns an array with the averages
 def calcDollar():
-    r = requests.get('https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/').json()
+    try:
+        r = requests.get('https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/').json()
+    except:
+        time.sleep(5)
+        calcDollar()
     USD = r["USD"]
     VEF = r["VEF"]
     avg_1h =  float(VEF["avg_1h"])  / float(USD["avg_1h"])
